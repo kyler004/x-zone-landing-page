1e969d221b07014afb69d5fe63fbe18fb0777c3a
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-02-23 00:00:00 +0100

    Implemented feature forward section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className="section__margin feature-section">
+      <div className="feature-image">
+        <img
+          src="man-working-using-virtual-reality-4019408-3337377@0 1.png"
+          alt="Promotional image 1"
+        />
+      </div>
+      <div className="feature-content">
+        <h1 className="text-3xl md:text-5xl font-bold text-text leading-snug">
+          We complete every projects <br /> extra care as customer need
+        </h1>
+        <p className="leading-relaxed tracking-wide">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at
+          dictum risus, <br /> non suscipit arcu. Quisque aliquam posuere
+          tortor, sit amet convallis nuncoe <br /> scelerisque in. orem ipsum
+          dolor sit amet, consectetur adipisicing elit. Labore <br /> eius
+          molestiae facere, natus reprehenderit eaque eum, autem ipsam. Magfini,{" "}
+          <br /> error. Tempora odit laborum iure inventore possimus laboriosam
+          qui nam.{" "}
+        </p>
+        <button className="btn-primary max-w-fit">Learn More</button>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <section className="section__margin">
+      <div className="newsletter-container">
+        <h2 className="heading-lg text-center">Newsletter</h2>
+        <p className="body-text text-center max-w-2xl mx-auto">
+          We turn information into actionable insights We work to understand
+          your issues and are driven to ask better questions in the pursuit of
+          making work.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Newsletter;
