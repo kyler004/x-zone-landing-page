202dd3f1c9a0621c29b14c9ef476e6b41b321faa
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-02-22 22:22:22 +0100

    Implement hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { PlayCircle } from "lucide-react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className=" section__padding flex justify-between items-center flex-wrap flex-col md:flex-row ">
+      <div className="section-container flex-1">
+        <h1 className="  heading-xl text-gradient mb-4">
+          Virtual Reality <br />
+          Business Solutions
+        </h1>
+        <p className=" body-text mb-4">
+          We have over 15 year exprience in business consultting arena. We have
+          over 15 year exprience in business consultting arena and artficial
+          intelligence.
+        </p>
+        <div className="flex gap-4 mt-10">
+          <button className="hero-cta btn-primary">Get Started</button>
+          <button className="btn-outline">
+            <PlayCircle className="mr-2" /> Watch video
+          </button>
+        </div>
+      </div>
+
+      <div className="flex-1">
+        <img
+          src="/Saly-19.png"
+          alt="Hero image"
+          className="w-full h-full object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
